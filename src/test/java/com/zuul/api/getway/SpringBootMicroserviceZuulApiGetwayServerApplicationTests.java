e9972341eb71b2e380c0babf7a8fbc105bd0f5f4
sandip vaghasiya <hi.shah31@gmail.com>
2019-12-16 10:23:55 +0100

    inital commit

---
+++ src/test/java/com/zuul/api/getway/SpringBootMicroserviceZuulApiGetwayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuul.api.getway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMicroserviceZuulApiGetwayServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
